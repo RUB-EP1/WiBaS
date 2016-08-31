4f1e42221938ef950eea4acb1c24bf5a4ea9217f
Julian Pychy <julian@pc17.ep1.rub.de>
2016-08-31 13:21:49 +0200

    Fixed an issue in the root start script

--- examples/aclic/root_startscript.cc
+++ examples/aclic/root_startscript.cc
@@ -1,6 +1,9 @@
 {
    gROOT->ProcessLine(".include ../../include");
+   gROOT->ProcessLine(".L ../../src/FastPointMap.cc+");
+   gROOT->ProcessLine(".L ../../src/PhasespaceCoord.cc+");
    gROOT->ProcessLine(".L ../../src/PhasespacePoint.cc+");
+   gROOT->ProcessLine(".L ../../src/PhasespacePointCloud.cc+");
    gROOT->ProcessLine(".L ../../src/WibFitFunction.cc+");
    gROOT->ProcessLine(".L ../../src/WibVoigtFitFunction.cc+");
    gROOT->ProcessLine(".L ../../src/WibasCore.cc+");
